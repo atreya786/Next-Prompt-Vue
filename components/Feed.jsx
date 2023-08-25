2146e43116cbdb7bc2908a8f07ed8560f3e4c763
Atreya Chandan <atreyachandan786@gmail.com>
2023-08-25 12:34:39 +0530

    Completed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 
-import PromptCard from "./promptCard";
+import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
